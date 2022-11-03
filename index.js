047312ad0470d4bce246a17ed3c5a7561e8a9a52
Jean Andrade <jeandeneves@outlook.com>
2022-11-03 17:50:57 -0300

    BodyParser adicionado e endpoints de criação e atualização criados

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 const port = 3000;
+
+app.use(bodyParser.json());
 
 app.get('/',(req, res) => {
   res.send('hello world');
@@ -10,18 +14,39 @@ app.get('/',(req, res) => {
 const mensagens = [
   'primeira mensagem','segunda mensagem'
 ];
-// - [GET] /mensagens - retorna a lista de mensagens
+
+// - [GET] /mensagens - Retorna a lista de mensagens
 app.get('/mensagens',(req, res) => {
   res.send(mensagens);
 });
 
-// - [GET] /mensagens/{id} - retorna apenas uma unica mensagem pelo id
+// - [GET] /mensagens/{id} - Retorna apenas uma unica mensagem pelo ID
 app.get('/mensagens:id',(req, res) => {
   const id = req.params.id - 1;
 
   const mensagem = mensagens[id];
 
   res.send(mensagem);
+});
+
+// - [post] /mensagens - Cria uma nova mensagens
+app.post('/mensagens',(req, res) => {
+  const mensagem = req.body.mensagem;
+
+  mensagens.push(mensagem);
+  
+  res.send(`Mensagem criada com sucesso: '${mensagem}'`);
+});
+
+// - [GET] /mensagens/{id} - Atualiza uma mensagem pelo ID
+app.put('/mensagens:id',(req, res) => {
+  const id = req.params.id - 1;
+
+  const mensagem = req.body.mensagem;
+
+  mensagens[id] = mensagem;
+
+  res.send(`Mensagem atualizada com sucesso: '${mensagem}.'`);
 });
 
 app.listen(port, () => {
