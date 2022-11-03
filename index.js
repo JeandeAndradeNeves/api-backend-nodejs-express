99f6972c77addb6f09a77c27b1fa712ecc752e67
Jean Andrade <jeandeneves@outlook.com>
2022-11-03 16:44:18 -0300

    Listagem de endpoints e criação dos endpoints de leitura (individual e de todos)

--- index.js
+++ index.js
@@ -4,7 +4,24 @@ const app = express();
 const port = 3000;
 
 app.get('/',(req, res) => {
-  res.send('Hello World');
+  res.send('hello world');
+});
+
+const mensagens = [
+  'primeira mensagem','segunda mensagem'
+];
+// - [GET] /mensagens - retorna a lista de mensagens
+app.get('/mensagens',(req, res) => {
+  res.send(mensagens);
+});
+
+// - [GET] /mensagens/{id} - retorna apenas uma unica mensagem pelo id
+app.get('/mensagens:id',(req, res) => {
+  const id = req.params.id - 1;
+
+  const mensagem = mensagens[id];
+
+  res.send(mensagem);
 });
 
 app.listen(port, () => {
